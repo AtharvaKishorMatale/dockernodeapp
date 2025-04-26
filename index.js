43e9bf411ee493955f9c7a71fec81dff763aca0b
AtharvaM <mataleatharva5@gmail.com>
2025-04-26 06:14:20 +0000

    commit3

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const app = express();
 // Respond with "Hello World" when a GET request is made to the root "/"
 
 app.get('/', (req, res) => {
-
-  res.send(' Hello from Nodejs App Atharva here hii');
+  res.send(' Hello from Nodejs App Atharva her');
 
 });
 
